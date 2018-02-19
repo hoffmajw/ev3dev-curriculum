647208d058cb105324d9c133157dd3b0e247a3d2
hoffmajw <hoffmajw@rose-hulman.edu>
2018-02-19 16:23:26 -0500

    m

--- projects/hoffmajwProject/hoffmajw_finalproject_ev3.py
+++ projects/hoffmajwProject/hoffmajw_finalproject_ev3.py
@@ -1,203 +1,343 @@
-
 import ev3dev.ev3 as ev3
+import robot_controller_final as robo
+import mqtt_remote_method_calls as com
 import time
-import robot_controller as robo
 
 
 def getCheeseburgers(robot):
+    """Instructs the cart to look for some cheeseburger ingredients, returns
+       the items in the cart"""
+
     print('--------------------------------------')
     print('Getting ingredients for cheeseburgers.')
     print('--------------------------------------')
 
-#    robot.move_forward(300, 300)
+    count = 0
 
     while True:
-        if robot.color_sensor.color is ev3.ColorSensor.COLOR_RED:
-            print('found red')
+        if len(robot.items_in_cart) == 5:
             robot.stop()
-            print('begin speaking')
-            ev3.Sound.speak('Tomato added to cart').wait()
-            time.sleep(.5)
-            ev3.Sound.beep().wait()
-            print('speaking finished')
+            break
+
+        robot.move_forward(300, 300)
+
+        if robot.color_sensor.color is ev3.ColorSensor.COLOR_RED:
+            robot.stop()
+
+            ev3.Sound.speak('Tomato added to cart')
+
+            time.sleep(5)
+
             robot.items_in_cart = robot.items_in_cart + ['Tomato']
 
             while robot.color_sensor.color is ev3.ColorSensor.COLOR_RED:
-                print()
-            #     robot.move_forward(300, 300)
-
-        # elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BROWN:
-        #     robot.stop()
-        #     ev3.Sound.speak('Ground beef added to cart.').wait()
-        #     robot.items_in_cart = robot.items_in_cart + ['Ground Beef']
-        #     robot.move_forward(300, 300)
+                robot.move_forward(300, 300)
 
         elif robot.color_sensor.color is ev3.ColorSensor.COLOR_GREEN:
-            print('Found green')
             robot.stop()
-            print('begin speaking')
-            ev3.Sound.speak('Lettuce added to cart').wait()
-            time.sleep(.5)
-            ev3.Sound.beep().wait()
-            print('finished speaking')
+
+            ev3.Sound.speak('Lettuce added to cart')
+
+            time.sleep(5)
+
             robot.items_in_cart = robot.items_in_cart + ['Lettuce']
 
             while robot.color_sensor.color is ev3.ColorSensor.COLOR_GREEN:
-                print()
-            #     robot.move_forward(300, 300)
+                robot.move_forward(300, 300)
 
-        # elif robot.color_sensor.color is ev3.ColorSensor.COLOR_YELLOW:
-        #     robot.stop()
-        #     ev3.Sound.speak('Mustard added to cart.').wait()
-        #     robot.items_in_cart = robot.items_in_cart + ['Mustard']
-        #     robot.move_forward(300, 300)
-        # elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLACK:
-        #     robot.stop()
-        #     ev3.Sound.speak('Onions added to cart.').wait()
-        #     robot.items_in_cart = robot.items_in_cart + ['Onions']
-        #     robot.move_forward(300, 300)
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_YELLOW:
+            robot.stop()
+
+            if count == 0:
+                ev3.Sound.speak('Ground beef added to cart.')
+
+                time.sleep(5)
+
+                robot.items_in_cart = robot.items_in_cart + ['Ground Beef']
+                count += 1
+            else:
+                ev3.Sound.speak('Cheese added to cart')
+
+                time.sleep(5)
+
+                robot.items_in_cart = robot.items_in_cart + ['Cheese']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_YELLOW:
+                robot.move_forward(300, 300)
+
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLACK:
+            robot.stop()
+
+            ev3.Sound.speak('Onions added to cart.')
+
+            time.sleep(5)
+
+            robot.items_in_cart = robot.items_in_cart + ['Onions']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_BLACK:
+                robot.move_forward(300, 300)
 
         elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLUE:
-            print('found blue')
             robot.stop()
-            print('begin speaking')
-            ev3.Sound.speak('Cheese added to cart').wait()
-            time.sleep(.5)
-            ev3.Sound.beep().wait()
-            print('finished speaking')
-            robot.items_in_cart = robot.items_in_cart + ['Cheese']
+
+            if count == 0:
+                ev3.Sound.speak('Ground beef added to cart.')
+
+                time.sleep(5)
+
+                robot.items_in_cart = robot.items_in_cart + ['Ground Beef']
+                count += 1
+            else:
+                ev3.Sound.speak('Cheese added to cart')
+
+                time.sleep(5)
+
+                robot.items_in_cart = robot.items_in_cart + ['Cheese']
 
             while robot.color_sensor.color is ev3.ColorSensor.COLOR_BLUE:
-                print()
-            #     robot.move_forward(300, 300)
+                robot.move_forward(300, 300)
 
-        if len(robot.items_in_cart) == 5:
+        elif robot.touch_sensor.is_pressed:
+            clearCart(robot)
+            leaveGrocery(robot)
             break
 
     return robot.items_in_cart
 
 
 def getSalad(robot):
+
+    """Instructs the cart to get some ingredients for a salad, returns the
+       items in the cart"""
     print('--------------------------------')
     print('Getting ingredients for a salad.')
     print('--------------------------------')
+    count = 0
+    while True:
+        if len(robot.items_in_cart) == 5:
+            robot.stop()
+            break
 
-    if robot.color_sensor.color is ev3.ColorSensor.COLOR_RED:
-        robot.stop()
-        ev3.Sound.speak('Tomato added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Tomato']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BROWN:
-        robot.stop()
-        ev3.Sound.speak('Croutons added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Croutons']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_GREEN:
-        robot.stop()
-        ev3.Sound.speak('Lettuce added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Lettuce']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_YELLOW:
-        robot.stop()
-        ev3.Sound.speak('Shredded cheese added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Shredded cheese']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLACK:
-        robot.stop()
-        ev3.Sound.speak('Salad dressing added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Salad dressing']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLUE:
-        robot.stop()
-        ev3.Sound.speak('Carrots added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Carrots']
-        robot.move_forward(900)
+        robot.move_forward(300, 300)
+
+        if robot.color_sensor.color is ev3.ColorSensor.COLOR_RED:
+            robot.stop()
+
+            ev3.Sound.speak('Tomato added to cart')
+
+            time.sleep(5)
+
+            robot.items_in_cart = robot.items_in_cart + ['Tomato']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_RED:
+                robot.move_forward(300, 300)
+
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_GREEN:
+            robot.stop()
+
+            ev3.Sound.speak('Lettuce added to cart')
+
+            time.sleep(5)
+
+            robot.items_in_cart = robot.items_in_cart + ['Lettuce']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_GREEN:
+                robot.move_forward(300, 300)
+
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_YELLOW:
+            robot.stop()
+
+            ev3.Sound.speak('Cheese added to cart.')
+
+            time.sleep(5)
+
+            robot.items_in_cart = robot.items_in_cart + ['Cheese']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_YELLOW:
+                robot.move_forward(300, 300)
+
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLACK:
+            robot.stop()
+
+            ev3.Sound.speak('Croutons added to cart.')
+
+            time.sleep(5)
+
+            robot.items_in_cart = robot.items_in_cart + ['Croutons']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_BLACK:
+                robot.move_forward(300, 300)
+
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLUE:
+            robot.stop()
+
+            if count == 0:
+                ev3.Sound.speak('Ranch added to cart.')
+
+                time.sleep(5)
+
+                robot.items_in_cart = robot.items_in_cart + ['Ranch']
+                count += 1
+            else:
+                ev3.Sound.speak('Cheese added to cart')
+
+                time.sleep(5)
+
+                robot.items_in_cart = robot.items_in_cart + ['Cheese']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_BLUE:
+                robot.move_forward(300, 300)
+
+        elif robot.touch_sensor.is_pressed:
+            clearCart(robot)
+            leaveGrocery(robot)
+            break
 
     return robot.items_in_cart
 
 
 def getPizza(robot):
+    """Instructs the robot to get som ingredients for a pizza, returns the
+       items in the cart"""
+
     print('--------------------------------')
     print('Getting ingredients for a pizza.')
     print('--------------------------------')
+    count = 0
+    while True:
+        if len(robot.items_in_cart) == 5:
+            robot.stop()
+            break
 
-    robot.move_forward(500, 500)
+        robot.move_forward(300, 300)
 
-    if robot.color_sensor.color is ev3.ColorSensor.COLOR_RED:
-        robot.stop()
-        ev3.Sound.speak('Tomato added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Tomato']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BROWN:
-        robot.stop()
-        ev3.Sound.speak('Croutons added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Croutons']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_GREEN:
-        robot.stop()
-        ev3.Sound.speak('Lettuce added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Lettuce']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_YELLOW:
-        robot.stop()
-        ev3.Sound.speak('Shredded cheese added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Shredded cheese']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLACK:
-        robot.stop()
-        ev3.Sound.speak('Salad dressing added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Salad dressing']
-        robot.move_forward(900)
-    elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLUE:
-        robot.stop()
-        ev3.Sound.speak('Carrots added to cart.').wait()
-        robot.items_in_cart = robot.items_in_cart + ['Carrots']
-        robot.move_forward(900)
+        if robot.color_sensor.color is ev3.ColorSensor.COLOR_RED:
+            robot.stop()
+
+            ev3.Sound.speak('Pepperoni added to cart')
+
+            time.sleep(5)
+
+            robot.items_in_cart = robot.items_in_cart + ['Pepperoni']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_RED:
+                robot.move_forward(300, 300)
+
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_GREEN:
+            robot.stop()
+
+            ev3.Sound.speak('Peppers added to cart')
+
+            time.sleep(5)
+
+            robot.items_in_cart = robot.items_in_cart + ['Peppers']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_GREEN:
+                robot.move_forward(300, 300)
+
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_YELLOW:
+            robot.stop()
+
+            ev3.Sound.speak('Cheese added to cart.')
+
+            time.sleep(5)
+
+            robot.items_in_cart = robot.items_in_cart + ['Cheese']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_YELLOW:
+                robot.move_forward(300, 300)
+
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLACK:
+            robot.stop()
+
+            ev3.Sound.speak('Crust added to cart.')
+
+            time.sleep(5)
+
+            robot.items_in_cart = robot.items_in_cart + ['Crust']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_BLACK:
+                robot.move_forward(300, 300)
+
+        elif robot.color_sensor.color is ev3.ColorSensor.COLOR_BLUE:
+            robot.stop()
+
+            if count == 0:
+                ev3.Sound.speak('Mushrooms added to cart.')
+
+                time.sleep(5)
+
+                robot.items_in_cart = robot.items_in_cart + ['Mushrooms']
+                count += 1
+            else:
+                ev3.Sound.speak('Cheese added to cart')
+
+                time.sleep(5)
+
+                robot.items_in_cart = robot.items_in_cart + ['Cheese']
+
+            while robot.color_sensor.color is ev3.ColorSensor.COLOR_BLUE:
+                robot.move_forward(300, 300)
+
+        elif robot.touch_sensor.is_pressed:
+            clearCart(robot)
+            leaveGrocery(robot)
+            break
 
     return robot.items_in_cart
 
 
-def readCart(items_in_cart):
-    print('----------------')
+def readCart(robot):
+    """Reads and prints the items in the cart"""
+
+    print('-------------')
     print('Reading cart.')
-    print('----------------')
+    print('-------------')
 
-    print(items_in_cart)
+    print(robot.items_in_cart)
 
-    for k in range(len(items_in_cart)):
-        ev3.Sound.speak(items_in_cart[k]).wait()
+    ev3.Sound.speak('You have these items in your cart.')
+    time.sleep(2.0)
+    for k in range(len(robot.items_in_cart)):
+        ev3.Sound.speak(robot.items_in_cart[k])
+        time.sleep(1.25)
 
 
-def leaveGrocery():
+def clearCart(robot):
+    """Removes all items from a cart, setting the cart's items to an empty
+    list of strings (the cart can only hold 5 items"""
+    print('--------------')
+    print('Clearing cart.')
+    print('--------------')
+
+    ev3.Sound.speak('Cart cleared. There is nothing in your cart.')
+    robot.items_in_cart = []
+
+
+def leaveGrocery(robot):
+    """Tells the user you are done at the grocery"""
+
     print('----------------')
     print('Leaving grocery.')
     print('----------------')
+
+    robot.turn_degrees(450, 900)
+    ev3.Sound.speak('All done at the grocery.')
 
 
 def main():
     cart = robo.Snatch3r()
 
     btn = ev3.Button()
+    btn.process()
+    if btn.backspace:
+        exit()
 
-    while True:
-        choice = input('"c" for cheeseburger, "s" for salad, "p" for pizza, '
-                       'or "cart" to see whats in your cart:    ')
-        print()
+    mqtt_j = com.MqttClient(cart)
+    mqtt_j.connect_to_pc()
 
-        if choice == 'c':
-            getCheeseburgers(cart)
-
-        elif choice == 's':
-            getSalad(cart)
-
-        elif choice == 'p':
-            getPizza(cart)
-
-        elif choice == 'cart':
-            readCart(cart.items_in_cart)
-
-        if btn.backspace:
-            break
+    cart.loop_forever()
 
 
-main()
+if __name__ == "__main__":
+    main()
--- projects/hoffmajwProject/hoffmajw_finalproject_pc.py
+++ projects/hoffmajwProject/hoffmajw_finalproject_pc.py
@@ -1,7 +1,14 @@
 import tkinter
+from tkinter import ttk
+import mqtt_remote_method_calls as com
 
 
 def main():
+    """Creates a GUI with a drop down menu, and buttons with keyboard
+       binding to move the robot."""
+    mqtt_j = com.MqttClient()
+    mqtt_j.connect_to_ev3()
+
     root = tkinter.Tk()
     root.geometry('300x300')
 
@@ -10,17 +17,164 @@ def main():
 
     orderMenu = tkinter.Menu(menu)
     menu.add_cascade(label='Order', menu=orderMenu)
+
     orderMenu.add_command(label='Cheeseburgers', font=('Times', '12'),
-                          command=getCheeseburgers(cart))
-    orderMenu.add_command(label='Salad', font=('Times', '12'), command=getSalad)
-    orderMenu.add_command(label='Pizza', font=('Times', '12'), command=getPizza)
+                          command=lambda: handle_cheeseburger(
+                              mqtt_j))
+    orderMenu.add_command(label='Salad', font=('Times', '12'),
+                          command=lambda: handle_salad(mqtt_j))
+    orderMenu.add_command(label='Pizza', font=('Times', '12'),
+                          command=lambda: handle_pizza(mqtt_j))
     orderMenu.add_separator()
     orderMenu.add_command(label='Cart', font=('Times', '12'),
-                          command=readCart(cart.items_in_cart))
+                          command=lambda: handle_cart(mqtt_j))
+    orderMenu.add_command(label='Clear Cart', font=('Times', '12'),
+                          command=lambda: handle_clear(mqtt_j))
     orderMenu.add_command(label='Leave Grocery', font=('Times', '12'),
-                          command=leaveGrocery)
+                          command=lambda: handle_leave(mqtt_j))
+
+    main_frame = ttk.Frame(root, padding=20, relief='raised')
+    main_frame.grid()
+
+    left_speed_label = ttk.Label(main_frame, text="Left")
+    left_speed_label.grid(row=0, column=0)
+    left_speed_entry = ttk.Entry(main_frame, width=8)
+    left_speed_entry.insert(0, "600")
+    left_speed_entry.grid(row=1, column=0)
+
+    right_speed_label = ttk.Label(main_frame, text="Right")
+    right_speed_label.grid(row=0, column=2)
+    right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
+    right_speed_entry.insert(0, "600")
+    right_speed_entry.grid(row=1, column=2)
+
+    forward_button = ttk.Button(main_frame, text="Forward")
+    forward_button.grid(row=2, column=1)
+    forward_button['command'] = lambda: forward_callback(mqtt_j,
+                                                         left_speed_entry,
+                                                         right_speed_entry)
+    root.bind('<Up>', lambda event: forward_callback(mqtt_j,
+                                                     left_speed_entry,
+                                                     right_speed_entry))
+
+    left_button = ttk.Button(main_frame, text="Left")
+    left_button.grid(row=3, column=0)
+    left_button['command'] = lambda: left_callback(mqtt_j,
+                                                   left_speed_entry,
+                                                   right_speed_entry)
+    root.bind('<Left>', lambda event: left_callback(mqtt_j,
+                                                    left_speed_entry,
+                                                    right_speed_entry))
+
+    stop_button = ttk.Button(main_frame, text="Stop")
+    stop_button.grid(row=3, column=1)
+    stop_button['command'] = lambda: stop_callback(mqtt_j, )
+    root.bind('<space>', lambda event: stop_callback(mqtt_j))
+
+    right_button = ttk.Button(main_frame, text="Right")
+    right_button.grid(row=3, column=2)
+    right_button['command'] = lambda: right_callback(mqtt_j,
+                                                     left_speed_entry,
+                                                     right_speed_entry)
+    root.bind('<Right>', lambda event: right_callback(mqtt_j,
+                                                      left_speed_entry,
+                                                      right_speed_entry))
+
+    back_button = ttk.Button(main_frame, text="Back")
+    back_button.grid(row=4, column=1)
+    back_button['command'] = lambda: back_callback(mqtt_j,
+                                                   left_speed_entry,
+                                                   right_speed_entry)
+    root.bind('<Down>', lambda event: back_callback(mqtt_j,
+                                                    left_speed_entry,
+                                                    right_speed_entry))
+
+    up_button = ttk.Button(main_frame, text="Up")
+    up_button.grid(row=5, column=0)
+    up_button['command'] = lambda: send_up(mqtt_j)
+    root.bind('<u>', lambda event: send_up(mqtt_j))
+
+    down_button = ttk.Button(main_frame, text="Down")
+    down_button.grid(row=6, column=0)
+    down_button['command'] = lambda: send_down(mqtt_j)
+    root.bind('<j>', lambda event: send_down(mqtt_j))
+
+    q_button = ttk.Button(main_frame, text="Quit")
+    q_button.grid(row=5, column=2)
+    q_button['command'] = (lambda: quit_program(mqtt_j, False))
+
+    e_button = ttk.Button(main_frame, text="Exit")
+    e_button.grid(row=6, column=2)
+    e_button['command'] = (lambda: quit_program(mqtt_j, True))
 
     root.mainloop()
+
+
+def handle_cheeseburger(mqtt):
+    mqtt.send_message('getCheeseburgers')
+
+
+def handle_salad(mqtt):
+    mqtt.send_message('getSalad')
+
+
+def handle_pizza(mqtt):
+    mqtt.send_message('getPizza')
+
+
+def handle_cart(mqtt):
+    mqtt.send_message('readCart')
+
+
+def handle_clear(mqtt):
+    mqtt.send_message('clearCart')
+
+
+def handle_leave(mqtt):
+    mqtt.send_message('leaveGrocery')
+
+
+def forward_callback(mqtt_client, left_speed_entry, right_speed_entry):
+    print("move_forward")
+    mqtt_client.send_message("move_forward", [int(left_speed_entry.get()),
+                                              int(right_speed_entry.get())])
+
+
+def left_callback(mqtt_client, left_speed_entry, right_speed_entry):
+    mqtt_client.send_message("turn_left", [int(left_speed_entry.get()),
+                                           int(right_speed_entry.get())])
+
+
+def stop_callback(mqtt_client):
+    mqtt_client.send_message("stop")
+
+
+def right_callback(mqtt_client, left_speed_entry, right_speed_entry):
+    mqtt_client.send_message("turn_right", [int(left_speed_entry.get()),
+                                            int(right_speed_entry.get())])
+
+
+def back_callback(mqtt_client, left_speed_entry, right_speed_entry):
+    mqtt_client.send_message("move_back", [int(left_speed_entry.get()),
+                                           int(right_speed_entry.get())])
+
+
+def send_up(mqtt_client):
+    print("arm_up")
+    mqtt_client.send_message("arm_up")
+
+
+def send_down(mqtt_client):
+    print("arm_down")
+    mqtt_client.send_message("arm_down")
+
+
+def quit_program(mqtt_client, shutdown_ev3):
+    if shutdown_ev3:
+        print("shutdown")
+        mqtt_client.send_message("shutdown")
+    mqtt_client.close()
+    exit()
 
 
 main()
